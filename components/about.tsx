edc34e66434c88ff1143f33af936ecd00688a674
Shaun <Hundin231@gmail.com>
2024-10-09 16:29:48 +0800

    Fix description of about page

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,29 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        Currently second-year undergraduate student studying {" "}
+        <span className="font-medium">Computer Science</span>, I love building tools and 
+        applications as such I am an avid hackathon participator. I am a {" "}
+        <span className="font-medium">full-stack app developer</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
+        creative problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          Java, Python, Dart, C, React, Next.js, Node.js, MongoDB, SQL
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also currently pursuing certifications in AI and Data analytics. I am always looking to
         learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
+        <span className="font-medium">full-time internship position</span> as a software
         developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I'm not coding</span>, I enjoy dancing, participating 
+        in physical activites such as running or sports and playing my guitar. I also enjoy{" "}
+        <span className="font-medium">learning new things</span> and enjoy reading I am
+        currently reading{" "}
+        <span className="font-medium">Stardust by Neil Gaiman</span>. 
       </p>
     </motion.section>
   );
